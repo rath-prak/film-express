bc968a64fbd390c8da6b6aecd3f4109214384c8a
rath-prak <rathanap@gmail.com>
2017-01-08 18:34:37 +1300

    play with cheerio

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const request = require('request');
 const path = require('path');
 const bodyParser = require('body-parser');
 const exphbs  = require('express-handlebars');
+const $ = require('cheerio');
 
 const getData = require('./controllers/getinfo')
 
@@ -14,7 +15,7 @@ app.set('port', process.env.PORT || 3000);
 const PORT = app.get('port');
 app.set('author', 'Rathania');
 
-app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'index', layoutsDir:__dirname + '/views/layouts'}));
+app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: '_layout', layoutsDir:__dirname + '/views/layouts'}));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
@@ -23,8 +24,8 @@ app.set('view engine', 'hbs')
 
 app.use(require('./controllers/index'));
 app.use(require('./controllers/getinfo'));
-app.use(require('./controllers/fullPlot'))
-
+app.use(require('./controllers/fullPlot'));
+app.use(require('./controllers/getActor'))
 
 app.listen(PORT, function(){
 	console.log(`Running on port ${PORT}...`);
---
+++ controllers/getActor.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const express = require('express');
+const request = require('request');
+const $ = require('cheerio');
+const router = express.Router();
+
+router.get('/actors', function(req, res){
+
+		const searchActor = $("#searchInput").val();
+		console.log(searchActor)
+	const actor = `Russell Crowe`;
+	const rating = 8;
+
+	res.render('./layouts/actors', {
+		title: actor,
+		popularity: rating
+	})
+
+})
+
+module.exports = router
+
+// 74d4d78f293267a7e850237f69ff3b2as
